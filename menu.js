2fd1a6da0a7c788211a27399a24f31bf605a9b93
rizanw <rzkandre@gmail.com>
2020-12-31 14:24:45 +0700

    logic menu bug fixed

--- menu.js
+++ menu.js
@@ -3,15 +3,10 @@ AFRAME.registerComponent("menu", {
 
   init: function () {
     var buttonEls = document.querySelectorAll(".button-menu");
-
+    this.buttonEls = buttonEls;
     this.onClick = this.onClick.bind(this);
     this.onMouseEnter = this.onMouseEnter.bind(this);
     this.onMouseLeave = this.onMouseLeave.bind(this);
-
-    // this.onMenuButtonClick = this.onMenuButtonClick.bind(this);
-    // for (var i = 0; i < buttonEls.length; ++i) {
-    //   buttonEls[i].addEventListener("click", this.onMenuButtonClick);
-    // }
 
     for (var i = 0; i < buttonEls.length; ++i) {
       buttonEls[i].addEventListener("mouseenter", this.onMouseEnter);
@@ -22,14 +17,18 @@ AFRAME.registerComponent("menu", {
 
   onClick: function (evt) {
     evt.target.pause();
-    evt.target.setAttribute("material", "color", "#046de7");
+    evt.target.setAttribute("material", "color", "white");
+    var buttonEls = this.buttonEls;
     this.el.addState("clicked");
+    for (var i = 0; i < buttonEls.length; ++i) {
+      buttonEls[i].object3D.scale.set(1.0, 1.0, 1.0);
+    } 
     evt.target.object3D.scale.set(1.2, 1.2, 1.2);
   },
 
   onMouseEnter: function (evt) {
     var buttonEls = this.buttonEls;
-    evt.target.setAttribute("material", "color", "#046de7");
+    evt.target.setAttribute("material", "color", "black");
     for (var i = 0; i < buttonEls.length; ++i) {
       if (evt.target === buttonEls[i]) {
         continue;
