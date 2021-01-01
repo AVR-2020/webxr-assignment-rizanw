4c3492b596ce63ca1cf7358c0f30d7b3237bd3a9
rizanw <rzkandre@gmail.com>
2021-01-01 20:50:02 +0700

    navigation menu done

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ AFRAME.registerComponent("menu", {
   schema: {},
 
   init: function () {
-    var buttonEls = document.querySelectorAll(".button-menu");
+    var buttonEls = document.querySelectorAll(".button-nav");
     this.buttonEls = buttonEls;
     this.onClick = this.onClick.bind(this);
     this.onMouseEnter = this.onMouseEnter.bind(this);
@@ -17,31 +17,62 @@ AFRAME.registerComponent("menu", {
 
   onClick: function (evt) {
     evt.target.pause();
-    evt.target.setAttribute("material", "color", "white");
-    var buttonEls = this.buttonEls;
     this.el.addState("clicked");
+    var buttonEls = this.buttonEls;
     for (var i = 0; i < buttonEls.length; ++i) {
-      buttonEls[i].object3D.scale.set(1.0, 1.0, 1.0);
-    } 
-    evt.target.object3D.scale.set(1.2, 1.2, 1.2);
+      buttonEls[i].lastElementChild.setAttribute(
+        "material",
+        "color",
+        "#013880"
+      );
+    }
+
+    var pos = evt.target.getAttribute("position");
+    var navBtnPos = { x: 0.07, y: 0.17 };
+    anime({
+      targets: ".button-nav",
+      update: function () {
+        evt.target.setAttribute("position", {
+          x: pos.x / 2,
+          y: pos.y / 2,
+          z: pos.z,
+        });
+      },
+    });
+    setTimeout(function () {
+      anime({
+        targets: ".button-nav",
+        update: function () {
+          evt.target.setAttribute("position", {
+            x: navBtnPos.x,
+            y: navBtnPos.y,
+            z: pos.z,
+          });
+        },
+      });
+      evt.target.lastElementChild.setAttribute("material", "color", "#FEC410");
+    }, 100);
+    var text = document.querySelector("#textHello");
+    if (text.getAttribute("visible")) {
+      text.setAttribute("visible", false);
+    } else {
+      text.setAttribute("visible", true);
+    }
   },
 
   onMouseEnter: function (evt) {
     var buttonEls = this.buttonEls;
-    evt.target.setAttribute("material", "color", "black");
     for (var i = 0; i < buttonEls.length; ++i) {
-      if (evt.target === buttonEls[i]) {
-        continue;
-      }
-      buttonEls[i].setAttribute("material", "color", "white");
+      buttonEls[i].firstElementChild.object3D.scale.set(1.0, 1.0, 1.0);
     }
+    evt.target.firstElementChild.object3D.scale.set(1.2, 1.2, 1.2);
   },
 
   onMouseLeave: function (evt) {
     if (this.el.is("clicked")) {
       return;
     }
-    evt.target.setAttribute("material", "color", "white");
+    evt.target.firstElementChild.object3D.scale.set(1.0, 1.0, 1.0);
   },
 
   onMenuButtonClick: function (evt) {
